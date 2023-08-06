f5a645ec9ea72fc5cb3d6f63c48596c5518d6c7d
RincewindWizzard <git@magierdinge.de>
2023-08-06 09:53:44 +0200

    dezentralised nodes

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,13 @@ mod tokenring;
 
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use std::time::Duration;
 use log::debug;
 use stderrlog::LogLevelNum;
 use threadpool::ThreadPool;
-use crate::tokenring::{Token, TokenRing};
-
-fn node(rx: Receiver<Token>, tx: Sender<Token>) {
-    let mut id = 0;
-    while let Ok(token) = rx.recv() {
-        match token {
-            Token::INIT(new_id) => {
-                id = new_id;
-                debug!("node {id:02}: initialized node");
-                tx.send(Token::INIT(id + 1)).unwrap();
-            }
-            _ => {
-                tx.send(token).unwrap();
-            }
-        }
-    };
-}
+use crate::tokenring::{Node, RouteType, Token};
+use crate::tokenring::Token::TEXT;
 
 fn main() {
     stderrlog::new()
@@ -32,14 +19,28 @@ fn main() {
         .init()
         .unwrap();
 
-    let mut ring = TokenRing::new(10, node);
-    ring.send(Token::TEXT(format!("Hello Token Ring!")));
-    ring.shutdown();
-
-
-    while let Ok(token) = ring.rx.recv() {
-        println!("Received: {token:?}");
+    // construct a token ring
+    let mut nodes = vec![Node::new()];
+    for i in 1..10 {
+        let node = Node::new();
+        nodes[i - 1].connect(&node).unwrap();
+        nodes.push(node);
     }
+    nodes.last().unwrap().connect(&nodes[0]).unwrap();
 
-    ring.join();
+
+    let node = &nodes[0];
+    node.init().unwrap();
+    thread::sleep(Duration::from_millis(1000));
+
+    let (tx, rx) = node.channel();
+    node.ping(5).unwrap();
+    thread::sleep(Duration::from_millis(1000));
+    let token = rx.recv().unwrap();
+    println!("Received {token:?}");
+    node.shutdown().unwrap();
+
+    for node in nodes {
+        node.join().unwrap();
+    }
 }
--- src/tokenring.rs
+++ src/tokenring.rs
@@ -1,87 +1,232 @@
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
-use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::thread::JoinHandle;
+use crossbeam_channel::{Receiver, RecvError, Sender, SendError};
+use log::debug;
+use crate::tokenring::Token::{ConnectRx, ConnectTx};
 
-#[derive(Debug)]
+type NodeId = usize;
+
+
+#[derive(Debug, Clone)]
 pub enum Token {
-    NUMBER(u64),
     TEXT(String),
     /// Initializing nodes with ids
     /// The intialized node saves its id and forwards INIT(id + 1) to the next node
     INIT(NodeId),
+    PING(NodeId, NodeId),
+    PONG(NodeId, NodeId),
+    ConnectTx(Sender<Token>),
+    ConnectRx(Receiver<Token>),
+    SHUTDOWN,
 }
 
-type Node = fn(Receiver<Token>, Sender<Token>);
-type NodeId = usize;
-
-pub struct TokenRing {
-    pool: ThreadPool,
-    pub rx: Receiver<Token>,
-    pub tx: Option<Sender<Token>>,
+fn is_internal(token: &Token) -> bool {
+    match token {
+        Token::TEXT(_) => { false }
+        Token::INIT(_) => { true }
+        Token::PING(_, _) => { false }
+        Token::PONG(_, _) => { false }
+        Token::SHUTDOWN => { true }
+        Token::ConnectTx(_) => { true }
+        Token::ConnectRx(_) => { true }
+    }
 }
 
-impl TokenRing {
-    pub fn new(count: usize, node: Node) -> TokenRing {
-        let pool = ThreadPool::new(count + 1);
+#[derive(PartialEq, Clone)]
+pub enum RouteType {
+    EXTERNAL,
+    INTERNAL,
+    BROADCAST,
+}
 
-        let mut rxs = vec![];
-        let mut txs = vec![];
-
-        for i in 0..count + 1 {
-            let (tx, rx) = mpsc::channel();
-            rxs.push(rx);
-            txs.push(tx);
-        }
-
-        let ring_tx = txs.remove(0);
-        let ring_rx = rxs.remove(rxs.len() - 1);
+pub trait NodeLogik {
+    fn run(node: Node);
+}
 
 
-        for i in 0..count {
-            let tx = txs.remove(0);
-            let rx = rxs.remove(0);
-            pool.execute(move || node(rx, tx));
-        }
+pub struct Node {
+    id: Option<NodeId>,
+    tx: Vec<(RouteType, Sender<Token>)>,
+    rx: Vec<(RouteType, Receiver<Token>)>,
+}
 
-        let (filter_out_tx, main_rx) = mpsc::channel();
+pub struct NodeHandle {
+    tx: Sender<Token>,
+    rx: Receiver<Token>,
+    thread: JoinHandle<()>,
+}
+
+impl NodeHandle {
+    pub fn join(self) -> std::thread::Result<()> {
+        self.thread.join()
+    }
+    pub fn channel(&self) -> (Sender<Token>, Receiver<Token>) {
+        (self.tx.clone(), self.rx.clone())
+    }
+    pub fn connect(&self, other: &NodeHandle) -> anyhow::Result<()> {
+        let (channel_tx, channel_rx) = crossbeam_channel::unbounded();
+        self.tx.send(ConnectTx(channel_tx))?;
+        other.tx.send(ConnectRx(channel_rx))?;
+        Ok(())
+    }
+    pub fn ping(&self, dst: NodeId) -> Result<(), SendError<Token>> {
+        self.tx.send(Token::PING(0, dst))
+    }
+    pub fn init(&self) -> Result<(), SendError<Token>> {
+        self.tx.send(Token::INIT(1))
+    }
+    pub fn shutdown(&self) -> Result<(), SendError<Token>> {
+        self.tx.send(Token::SHUTDOWN)
+    }
+}
 
 
-        pool.execute(move || {
-            while let Ok(token) = ring_rx.recv() {
-                match token {
-                    Token::INIT(_) => {}
-                    _ => {
-                        filter_out_tx.send(token).unwrap();
-                    }
-                }
-            }
+impl Node {
+    pub fn new() -> NodeHandle {
+        let (external_out_tx, external_out_rx) = crossbeam_channel::unbounded();
+        let (external_in_tx, external_in_rx) = crossbeam_channel::unbounded();
+        let mut node = Node {
+            id: None,
+            tx: vec![(RouteType::EXTERNAL, external_out_tx)],
+            rx: vec![(RouteType::EXTERNAL, external_in_rx)],
+        };
+
+        let handle = thread::spawn(move || {
+            node.run()
         });
 
 
-        let main_tx = ring_tx;
+        NodeHandle {
+            tx: external_in_tx,
+            rx: external_out_rx,
+            thread: handle,
+        }
+    }
 
-        let tokenring = TokenRing {
-            pool,
-            rx: main_rx,
-            tx: Some(main_tx),
+    fn node_name(&self) -> String {
+        if let Some(id) = self.id {
+            format!("node {id}")
+        } else {
+            format!("unknown node")
+        }
+    }
+
+    fn run(&mut self) {
+        while let Ok((src, token)) = self.recv() {
+            match token {
+                Token::INIT(new_id) => {
+                    if let None = self.id {
+                        self.id = Some(new_id);
+                        debug!("initialized {}", self.node_name());
+                        self.forward(Token::INIT(new_id + 1), RouteType::INTERNAL).unwrap();
+                    } else {
+                        self.forward(token, RouteType::INTERNAL).unwrap();
+                    }
+                }
+                Token::ConnectRx(rx) => {
+                    self.rx.push((RouteType::INTERNAL, rx));
+                }
+                Token::ConnectTx(tx) => {
+                    if let Some(id) = self.id {
+                        tx.send(Token::INIT(id + 1)).unwrap();
+                    }
+
+                    self.tx.push((RouteType::INTERNAL, tx));
+                }
+                Token::SHUTDOWN => {
+                    debug!("Shutdown {}", self.node_name());
+                    self.forward(token, RouteType::INTERNAL).unwrap();
+                    break;
+                }
+                Token::PING(src, dst) => {
+                    debug!("{} forwarded ping to dst {dst}", self.node_name());
+                    if let Some(id) = self.id {
+                        let src = if src == 0 {
+                            id
+                        } else { src };
+
+                        if id == dst {
+                            let (src, dst) = (dst, src);
+                            self.forward(
+                                Token::PONG(src, dst),
+                                if dst == id { RouteType::EXTERNAL } else { RouteType::INTERNAL },
+                            ).unwrap();
+                        } else {
+                            self.forward(
+                                Token::PING(src, dst),
+                                if dst == id { RouteType::EXTERNAL } else { RouteType::INTERNAL },
+                            ).unwrap();
+                        }
+                    } else {
+                        self.forward(token, RouteType::INTERNAL).unwrap();
+                    }
+                }
+                Token::PONG(src, dst) => {
+                    debug!("{} forwarded pong to dst {dst}", self.node_name());
+                    if let Some(id) = self.id {
+                        if dst == id {
+                            self.forward(token, RouteType::EXTERNAL).unwrap();
+                        } else {
+                            self.forward(token, RouteType::INTERNAL).unwrap();
+                        }
+                    }
+                }
+                _ => {
+                    debug!("{} forwards {token:?}", self.node_name());
+                    let dst = if src == RouteType::INTERNAL || src == RouteType::BROADCAST {
+                        RouteType::BROADCAST
+                    } else {
+                        RouteType::INTERNAL
+                    };
+                    self.forward(token, dst).unwrap();
+                }
+            }
         };
-        tokenring.send(Token::INIT(1));
-        tokenring
     }
 
-    pub fn shutdown(&mut self) {
-        self.tx = None;
+    pub fn forward(&mut self, token: Token, dst: RouteType) -> Result<(), anyhow::Error> {
+        let mut failed = vec![];
+        for (i, (route_type, tx)) in self.tx.iter().enumerate() {
+            if dst == RouteType::BROADCAST || dst == *route_type {
+                let result = tx.send(token.clone());
+                if let Err(SendError(token)) = result {
+                    debug!("{} Remove tx index {}", self.node_name(), i);
+                    failed.push(i);
+                }
+            }
+        }
+
+        failed.reverse();
+        for i in failed {
+            self.tx.remove(i);
+        }
+        Ok(())
     }
 
-    pub fn send(&self, token: Token) {
-        let tx = self.tx.as_ref().unwrap();
-        tx.send(token).unwrap();
+    pub fn send(&mut self, token: Token) -> Result<(), anyhow::Error> {
+        let dst = if is_internal(&token) {
+            RouteType::INTERNAL
+        } else {
+            RouteType::BROADCAST
+        };
+        self.forward(token, dst)
     }
 
-    pub fn join(&self) {
-        self.pool.join()
+    pub fn recv(&self) -> Result<(RouteType, Token), RecvError> {
+        let mut sel = crossbeam_channel::Select::new();
+
+        for (route_type, rx) in &self.rx {
+            sel.recv(&rx);
+        }
+        // Complete the selected operation.
+        let oper = sel.select();
+        let index = oper.index();
+        let (route_type, rx) = &self.rx[index];
+        let route_type = route_type.clone();
+
+        let token = oper.recv(rx)?;
+        Ok((route_type, token))
     }
 }
-
-
 
